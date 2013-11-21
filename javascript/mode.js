a3bb19684a82a1a73cca153bee780315d4e69665
mobcomics <tomi.huttula@gmail.com>
2013-11-20 23:23:37 -0800

    orientation change

--- javascript/mode.js
+++ javascript/mode.js
@@ -1,7 +1,12 @@
 $(window).load(function() { init(); });
 
 function init() {
+	window.addEventListener("orientationchange", checkOrientation, false);
 //	alert($("#actualFrame").width);
 	$("#actualFrame").css("width", $("#body").innerWidth()+"px");
 	$("#actualFrame").css("height", "1000px");
+}
+
+function checkOrientation () {
+	$("#actualFrame").css("width", $("#body").innerWidth()+"px");	
 }
