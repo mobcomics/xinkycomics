02b9c03ee877dbaf024b5ab41cb6e716abb7d8c5
mobcomics <tomi.huttula@gmail.com>
2013-11-20 22:17:37 -0800

    zooming

--- javascript/mode.js
+++ javascript/mode.js
@@ -2,4 +2,6 @@ $(window).load(function() { init(); });
 
 function init() {
 //	alert($("#actualFrame").width);
+//	$("#actualFrame").css("zoom", ".5");
+//	iframe.style.width = (origWidth * size) + 'px';
 }
