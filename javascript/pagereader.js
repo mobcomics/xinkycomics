83737950d6031f3d4e4c1c51a071b03ef5e357b1
mobcomics <tomi.huttula@gmail.com>
2013-11-22 14:18:29 -0800

    pagereader

--- javascript/pagereader.js
+++ javascript/pagereader.js
@@ -1,5 +1,5 @@
 $(window).load(function() { init(); });
-window.onresize = resizeIframe;
+// window.onresize = resizeIframe;
 var windowLoaded = false;
 var comicDataLoaded = false;
 var comicFolder;
@@ -10,40 +10,10 @@ function init() {
 	if(!window.console){ window.console = {log: function(){} }; } 
 	console.log("windowLoaded");
 	windowLoaded = true;
-	$("#actualFrame").css("height", "1000px");
-	resizeIframe();
-	$("#actualFrame").css("visibility", "block");
-	var c = comics.comicsList[(gup("comic")) ? gup("comic") : 0];
-	currentComic = ""+c+"";	
-	$("#actualFrame").attr("src", c.creatorUrl);
-	comicFolder = c.folderUrl;
 	if (comicDataLoaded) continueInit();
 }
 
 function continueInit() {
-	var p = myComic.panels[(browserStoragePanelNumber() <= 0) ? 0 : browserStoragePanelNumber()-1];
-	$("#pageThumbnail").attr("src", p.pimage);
-	var i = new Image();
-	i.src = p.pimage;
-	i.onload = function(){    
-	    imageLoaded(i, p);
-	};
-}
-
-function imageLoaded(i, p) {
-	var x = i.width/1000;
-	var scale = 130/(p.subImage.height);
-	$("#panelThumbnail").css({
-		zoom: scale,
-		width: x*p.subImage.width,
-		height: x*p.subImage.height,
-		background: "url('" + p.pimage + "')",
-		backgroundPosition: -x*p.subImage.x+"px " + -x*p.subImage.y + "px"
-	});	
-}
-
-function resizeIframe() {
-	$("#actualFrame").css("width", $("#body").innerWidth()+"px");	
 }
 
 function gup( name ) {
@@ -61,7 +31,6 @@ function loadScript(){
 	if(!window.console){ window.console = {log: function(){} }; } 
 	console.log("comicLoaded");
 	var c = comics.comicsList[(gup("comic")) ? gup("comic") : 0];
-//	comicFolder = comics.comicsList[gup('comic')].folderUrl;
 	comicFolder = c.folderUrl;
     var script = document.createElement("script");
     script.type = "text/javascript";
@@ -69,8 +38,7 @@ function loadScript(){
 		comicDataLoaded = true;
 		if (windowLoaded) continueInit();
     };
-//    script.src = comicFolder+""+comics.comicsList[gup('comic')].dataFile;
-  script.src = comicFolder+""+c.dataFile;
+    script.src = comicFolder+""+c.dataFile;
     document.getElementsByTagName("head")[0].appendChild(script);
 }
 
